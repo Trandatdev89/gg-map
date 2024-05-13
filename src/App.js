19670ca0f83aa0bd6ab2a74a73cfdd7027d3fb4d
tran quoc dat <tranquocdat30122004@gmail.com>
2024-05-13 23:34:42 +0700

    upload map

--- src/App.js
+++ src/App.js
@@ -4,47 +4,34 @@ import "./css/style.css";
 import { useDispatch, useSelector } from "react-redux";
 import { Change } from "./Action";
 import anh1 from "./img/duan24h.net_BĐQHSDĐ_Dong_Anh_HN_2030.jpg";
+import { useEffect, useState } from "react";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
 
 function App() {
-  const dispatch=useDispatch();
-  const kk=useSelector(state=>state.ChangeSlider);
+ 
   const style = {
     display: "inline-block",
     height: 100,
+    
   };
+  const [range,setRange]=useState(0);
+  useEffect(()=>{
+    const kk=document.querySelector("#kk");
+    kk.style.opacity=range/100;
+    
+  },[range])
 
-  const handleChange = (e) => {
-    dispatch(Change(e));
-  };
-
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627,
-    },
-    zoom: 11,
-  };
   return (
     <>
 
-      <div className="Map__slider" style={style}>
-        <Slider vertical defaultValue={0} onChange={handleChange} />
-      </div>
+      
+      <input type="range" className="Map__slider" defaultValue={0} id="slider" onChange={(e)=>setRange(e.target.value)}  style={{style}}/>
 
       <div className="Map">
-        <div className={kk?("Map-1 transparent"):("Map-1 opaque")}>
-
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "" }}
-          defaultCenter={defaultProps.center}
-          defaultZoom={defaultProps.zoom}
-        >
-          <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
-        </GoogleMapReact>
-        </div>
-        <img src={anh1} alt="loading..." className={kk?("opaque"):("transparent")}/>
+        
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7448.765276526692!2d105.84095344143087!3d21.017370372516254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab8f79019205%3A0xfd0f67139e1d0b7b!2sHanoi%20218%20Le%20Duan!5e0!3m2!1svi!2s!4v1715617304584!5m2!1svi!2s" style={{minHeight:"100vh",width:"100%"}}></iframe>
+        <img src={anh1} alt="loading..." id="kk"/>
       </div>
     </>
   );
